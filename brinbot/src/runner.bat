64459afc30ae6f0fec630e315638777573465dac
Shrewbi <Minichili@hotmail.com>
2016-07-13 13:53:38 +0200

    Update runner.bat

--- brinbot/src/runner.bat
+++ brinbot/src/runner.bat
@@ -1,3 +1,5 @@
+:: Runner for the bot. Compiles and runs main with the correct libraries. This should work on linux as well (not tested)
+
 javac -cp .;"../lib/mysql-connector-java-5.1.27.jar";"../lib/jsoup-1.9.1.jar"; csgoscraper/Database.java
 javac -cp .;"../lib/mysql-connector-java-5.1.27.jar";"../lib/jsoup-1.9.1.jar"; csgoscraper/Csglscraper.java
 javac -cp .;"../lib/mysql-connector-java-5.1.27.jar";"../lib/jsoup-1.9.1.jar"; csgoscraper/Main.java
